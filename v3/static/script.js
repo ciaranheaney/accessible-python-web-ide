f90d2d13824b8160e6bb7d498dd55eea66637f5d
ciaranheaney <ciaranheaney9@gmail.com>
2025-07-05 22:40:06 -0400

    Fixes for stop button

--- v3/static/script.js
+++ v3/static/script.js
@@ -96,6 +96,10 @@ async function run_code() {
     stop_button.addEventListener('click', function(event) {
         if (run_button.classList.contains('stop-btn')) {
             console.log('[STOPPED] code execution...')
+            run_button.classList = ['btn run-btn'];
+            run_button_label.textContent = 'Run Code';
+            run_button_icon.classList = ['fas fa-play'];
+            return
         }
     });
 
